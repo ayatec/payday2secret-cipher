3556a2108e112cd7f6237f89faf51b742912be07
ayaya <aya@ayatec.jp>
2018-11-11 23:05:27 +0900

    Add cipher output area

---
+++ src/js/components/CipherOutputArea.js
@@ -0,0 +1,68 @@
+
+// Import
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// Component
+const CipherOutputAreaComponent = ({ text }) => {
+  return (
+    <Style>
+      <TextArea>
+        <TextAreaLeft>
+          {text}
+        </TextAreaLeft>
+      </TextArea>
+      <TextArea>
+        <TextAreaRight>
+          {text}
+        </TextAreaRight>
+      </TextArea>
+    </Style>
+  );
+};
+
+// Style
+const Style = styled.div`
+  display: flex;
+`;
+
+const TextArea = styled.div`
+  margin: 1rem;
+  width: 50%;
+  min-height: 2rem;
+  border-radius: .5rem;
+  text-align: center;
+  overflow: auto;
+  background: #fff;
+  box-shadow: 2px 2px rgba(0,0,0,0.4);
+`;
+
+const TextAreaLeft = styled.pre`
+  font-family: "pd-secret";
+`;
+
+const TextAreaRight = styled.pre`
+  font-family: "pd-secret";
+  direction: rtl;
+  unicode-bidi: bidi-override;
+`;
+
+// PropTypes
+CipherOutputAreaComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+// Redux
+const mapStateToProps = (state) => {
+  return {
+    text: state.inputArea.text,
+  };
+};
+
+const CipherOutputArea = connect(
+  mapStateToProps,
+)(CipherOutputAreaComponent);
+
+export default CipherOutputArea;
