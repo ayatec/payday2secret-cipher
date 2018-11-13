765e0b6f556d81e05990f65a384725f4e6a10051
ayaya <aya@ayatec.jp>
2018-11-13 23:15:44 +0900

    Fix action name

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,7 @@
 
 export const INPUT_KEY = 'INPUT_KEY';
 export const INPUT_LINE_BREAK = 'INPUT_LINE_BREAK';
-export const INPUT_SPACE = 'INPUT_Space';
+export const INPUT_SPACE = 'INPUT_SPACE';
 export const INPUT_BACK = 'INPUT_BACK';
 export const CHANGE_INPUT = 'CHANGE_INPUT';
 export const CHANGE_INPUT_MODE = 'CHANGE_INPUT_MODE';
