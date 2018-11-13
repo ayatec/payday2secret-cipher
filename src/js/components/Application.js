f9fad87e0c30e82bd088ea147a7daa3d158d5ba9
ayaya <aya@ayatec.jp>
2018-11-13 23:35:33 +0900

    Remove window event listener

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -1,74 +1,39 @@
 
 // React
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
-
-import {
-  inputKey,
-  inputLineBreak,
-  inputBack,
-  inputSpace,
-} from '../actions';
 
 // App
 import ApplicationMain from './ApplicationMain';
 
-const ApplicationComponent = ({ dispatch }) => {
-  const handleKeyPress = (e) => {
-    console.log('e.keyCode', e.keyCode);
-    switch (e.keyCode) {
-      case 8:
-        dispatch(inputBack());
-        break;
-
-      case 13:
-        dispatch(inputLineBreak());
-        break;
-
-      case 32:
-        dispatch(inputSpace());
-        break;
-
-      default:
-        if (44 <= e.keyCode && e.keyCode <= 90) {
-          dispatch(inputKey(e.key));
-        }
-    }
-  };
-
-  document.addEventListener('keydown', handleKeyPress);
-
-  return (
-    <Style>
-      <Header>PAYDAY 2 Secret Cipher Decryption</Header>
-      <Main>
-        <ApplicationMain />
-      </Main>
-      <Footer>
-        <FooterGroup>
-          <FooterText>pd-secret font from </FooterText>
-          <FooterLink
-            target="_blank"
-            href="https://sites.google.com/view/nanriu"
-          >
-            Nanriu
-          </FooterLink>
-        </FooterGroup>
-        <FooterGroup>
-          <FooterText>powered by </FooterText>
-          <FooterLink
-            target="_blank"
-            href="https://mmra.me"
-          >
-            mmra.me
-          </FooterLink>
-        </FooterGroup>
-      </Footer>
-    </Style>
-  );
-};
+const Application = () => (
+  <Style>
+    <Header>PAYDAY 2 Secret Cipher Decryption</Header>
+    <Main>
+      <ApplicationMain />
+    </Main>
+    <Footer>
+      <FooterGroup>
+        <FooterText>pd-secret font from </FooterText>
+        <FooterLink
+          target="_blank"
+          href="https://sites.google.com/view/nanriu"
+        >
+          Nanriu
+        </FooterLink>
+      </FooterGroup>
+      <FooterGroup>
+        <FooterText>powered by </FooterText>
+        <FooterLink
+          target="_blank"
+          href="https://mmra.me"
+        >
+          mmra.me
+        </FooterLink>
+      </FooterGroup>
+    </Footer>
+  </Style>
+);
 
 // Style
 const Style = styled.div`
@@ -109,11 +74,5 @@ const FooterLink = styled.a`
   text-shadow: 2px 0 0 #561e78, -1px 0 0 #561e78, 0 2px 0 #561e78, 0 -1px 0 #561e78;
   }
 `;
-
-ApplicationComponent.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-};
-
-const Application = connect()(ApplicationComponent);
 
 export default Application;
